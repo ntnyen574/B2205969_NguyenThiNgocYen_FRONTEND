236422a0d1b6760d2e86d5de28f42a1f923ccb1b
ntnyen574 <yenb2205969@student.ctu.edu.vn>
2025-10-28 23:59:00 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <a href="/" class="navbar-brand ms-3">Ứng dụng Quản lý danh bạ</a>
+    <div class="navbar-nav ms-auto me-3">
+      <li class="nav-item">
+        <router-link :to="{ name: 'contactbook' }" class="nav-link">
+          Danh bạ <i class="fas fa-address-book"></i>
+        </router-link>
+      </li>
+    </div>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  width: 100%;
+}
+</style>
